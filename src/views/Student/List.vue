d44a544a921760b2e3f6b588eb3de235ebabd5df
Estevão Simães <estevaosimoes@MacBook-Air-de-Estevao.local>
2022-05-06 16:12:11 -0300

    adding delete integration with the API

--- src/views/Student/List.vue
+++ src/views/Student/List.vue
@@ -78,11 +78,11 @@
                 </v-dialog>
                 <v-dialog v-model="dialogDelete" max-width="500px">
                   <v-card>
-                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                    <v-card-title class="text-h5">Tem certeza que deseja deletar o aluno?</v-card-title>
                     <v-card-actions>
                       <v-spacer></v-spacer>
-                      <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                      <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                      <v-btn color="blue darken-1" text @click="closeDelete">Cancelar</v-btn>
+                      <v-btn color="blue darken-1" text @click="deleteItemConfirm">Sim</v-btn>
                       <v-spacer></v-spacer>
                     </v-card-actions>
                   </v-card>
@@ -202,11 +202,17 @@ export default {
       this.editedIndex = this.students.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialogDelete = true
+      console.log({'deleteItem()': this.editedIndex})
     },
 
     deleteItemConfirm() {
-      this.students.splice(this.editedIndex, 1)
-      this.closeDelete()
+      
+      api.delete(`student/${this.editedItem.id}`).then( () => {
+        this.students.splice(this.editedIndex, 1)
+        console.log('insideApi', this.editedItem.id)
+        this.closeDelete()
+      });
+
     },
 
     close() {
